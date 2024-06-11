a68c60614ba7f3e348d5c97745509cda448f046f
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-06-11 23:15:27 +0200

    feature: create BaseModel, DeviceType and Device

--- warehouse/models.py
+++ warehouse/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from utils.base_model import BaseModel
 
-# Create your models here.
+
+class DeviceType(BaseModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Device Type"
+        verbose_name_plural = "Device Types"
+
+
+class Device(BaseModel):
+    name = models.CharField(max_length=100)
+    quantity = models.IntegerField(default=0)
+    device_type = models.ForeignKey(DeviceType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Device"
+        verbose_name_plural = "Devices"
