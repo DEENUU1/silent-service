6d066020a4ebbf26f1928e56dcc9d5372c1c5715
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-06-11 23:17:39 +0200

    feature: register new models in  admin.py

--- warehouse/admin.py
+++ warehouse/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from warehouse.models import Device, DeviceType
 
-# Register your models here.
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'device_type')
+    list_filter = ('device_type',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(DeviceType)
